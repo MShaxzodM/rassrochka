3ae60c3bf1794caabd6f4ef0dd92acc32acb43de
Lochin Rohimboyev <mshaxzodm@example.com>
2023-06-15 17:40:36 +0500

    zvsczc

--- routes/delete.ts
+++ routes/delete.ts
@@ -4,17 +4,27 @@ import { deleteFile } from "../aws/s3";
 const del = Router();
 
 del.delete("/:id", async (req, res) => {
-    const id = req.params.id;
+    try {
+        const id = req.params.id;
 
-    const images = await db("images").select("filename").where("user_id", id);
-    images.map(async (image) => {
-        await deleteFile(image.filename);
-    });
-    await db("payments").where("user_id", id).del();
-    await db("pay_table").where("user_id", id).del();
-    await db("images").where("user_id", id).del();
-    await db("customers").where("id", id).del();
-    res.send("Object deleted successfully");
+        const images = await db("images")
+            .select("filename")
+            .where("user_id", id);
+        images.map(async (image) => {
+            try {
+                await deleteFile(image.filename);
+            } catch {
+                return false;
+            }
+        });
+        await db("payments").where("user_id", id).del();
+        await db("pay_table").where("user_id", id).del();
+        await db("images").where("user_id", id).del();
+        await db("customers").where("id", id).del();
+        res.send("Object deleted successfully");
+    } catch {
+        res.send("cant delete user");
+    }
 });
 
 export default del;
